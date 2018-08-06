3c7bef7bcaed7ce35684257a0ecb70d9fe15f5b1
kimisaak <kimisaak0@gmail.com>
2018-08-06 17:48:50 +0900

    출력 위치 변경

---
+++ study/KGCA/6a_API/Project1/Header.h
@@ -0,0 +1,12 @@
+#pragma once
+#define WIN32_LEAN_AND_MEAN //현재 사용하지 않는 MFC관련 내용을 포함시키지 않는다.
+
+//윈도우 헤더 파일
+#include <windows.h>
+
+//C언어 헤더 파일
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+
---
+++ study/KGCA/6a_API/Project1/WinMain.cpp
@@ -0,0 +1,5 @@
+#include "Header.h"
+
+//전역 변수
+HINSTANCE g_hInst;   ///현재 윈도우의 인스턴스
+WCHAR
