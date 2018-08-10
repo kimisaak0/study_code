c6b3db788008a508f81bbd3fdd2805324a8245cd
kimisaak <kimisaak0@gmail.com>
2018-08-10 16:46:27 +0900

    AbstractFactoryFattern

--- study/KGCA/5_DesignPattern/1_AbstractFactory/afHeader.h
+++ study/KGCA/5_DesignPattern/1_AbstractFactory/afHeader.h
@@ -3,44 +3,51 @@
 // AF는 객체를 생성할 때마다 동일한 제품군에 속하는 개체를 생성하기 위해 일일이 조건을 검사할 필요가 없다.
 // 또한 새로운 제품군을 생성할려고 할 경우 기존 소스코드와는 독립적으로 새로운 제품군을 추가하는 것이 가능하다.
 
+// 맵 생성에서는 서로 다른 컨셉의 맵을 만드는 팩토리를 만드는데 사용할 수 있다.
+
 #include <iostream>
 #include <vector>
 using std::vector;
 
 enum Direction {north, east, south, west};
 
+//미로의 모든 구성 요소들이 공통적으로 가져야 하는 기본 연산을 정의한 추상 클래스.
+//미로의 모든 구성 요소들은 이 클래스를 상속받아서 만들어진다.
 class MapSite {
 public:
+	//자식 클래스들이 이 연산을 재정의하도록 강제한다.
+	//즉 인터페이스만 정의하고 실제 작동은 정의하지 않은 것.
+	// Enter이므로 그 구성요소로 들어가려고 하는 연산이다.
 	virtual void Enter() = 0;
 };
 
+//----------------기본적인 미로의 구성 요소들.  시작
+
+//기본적인 방.
 class Room : public MapSite
 {
 private:
-	MapSite* m_pSides[4];
-	int      m_iRoomNum;
+	MapSite* m_pSides[4];       //방은 네개의 방향을 갖고 있고, 각 방향에는 미로의 다른 구성 요소가 올 수 있다.
+	int      m_iRoomNum;        //방 번호
 
 public:
-	void Enter() override {}
+	void Enter() override {}   // 기본 연산 재정의 (를 해야한다..)
 
-	MapSite* getSide(Direction dir) const {}
-	void     setSide(Direction dir, MapSite* pMapSize)
+	MapSite* getSide(Direction dir) const { return m_pSides[dir]; } // 지정한 방향에 무엇이 있는지를 반환하는 메소드.
+
+	void     setSide(Direction dir, MapSite* pMapSize)              // 지정한 방향에 무엇을 둘것인지를 결정하는 메소드.
 	{
 		m_pSides[dir] = pMapSize;
 	}
 
 public:
-	Room() {}
-
-	Room(int RoomNum)
-	{
-		m_iRoomNum = RoomNum;
-	}
+//	Room() {} // 자동으로 방 번호를 갖게 할 수도 있을 것이다.
+	Room(int RoomNum) {	m_iRoomNum = RoomNum;}  // 방 번호를 지정하여 생성하는 생성자.
 
 	~Room() {}
 };
 
- 
+ //기본적인 벽.
 class Wall : public MapSite
 {
 public:
@@ -51,39 +58,83 @@ public:
 	~Wall() {}
 };
 
+//기본적인 문.
 class Door : public MapSite
 {
 private:
-	Room* m_pRoom1;
-	Room* m_pRoom2;
-	bool  m_bIsOpen;
+	Room* m_pRoom1;         //연결된 방1
+	Room* m_pRoom2;         //연결된 방2
+	bool  m_bIsOpen;        //문이 열려있는지.
 
 public:
-	void Enter() override {}
-	Room* OtherSideFrom(Room* a) {}
+	void Enter() override {} //현재 방에서 다음 방으로 이동하는 연산이 정의되어야 한다.
+	Room* OtherSideFrom(Room* a) {} //현재 방을 넘겨주면 다음 방을 리턴해야 한다.
 
 public:
-	Door() {}
-
-	Door(Room* a, Room* b) 
-	{
-		m_pRoom1 = a;
-		m_pRoom2 = b;
-	}
+	//Door() {}
+	
+	//문은 두 방 사이에 있어야 한다. 
+	Door(Room* a, Room* b) {m_pRoom1 = a; m_pRoom2 = b;	} 
 
 	~Door() {}
 
 };
 
+//위의 구성 요소들을 복합하여 미로를 정의하는 클래스
 class Maze
 {
-public:
-	vector<Room*> m_RoomList;
+private:
+	vector<Room*> m_RoomList;  //방들을 벡터로 관리한다.
 	
-	void AddRoom(Room* room) { m_RoomList.push_back(room); }
-	Room* Roomnum(int num) const { return m_RoomList[num]; }
+public:
+	void AddRoom(Room* room) { m_RoomList.push_back(room); }  //방을 추가하는 메소드
+	Room* Roomnum(int num) const { return m_RoomList[num]; }  //방 번호로 방을 검색하는 메소드.
 
 public:
 	Maze() {}
 	~Maze() {}
 };
+
+//----------------기본적인 미로의 구성 요소들.  끝
+
+
+
+//----------------마법에 걸린 방 구성 요소들. 시작
+class Spell {
+public:
+	Spell() {}
+	~Spell() {}
+};
+
+class EnchantedRoom : public Room
+{
+public:
+	EnchantedRoom(int n, Spell* sp) : Room(n) {}
+};
+
+class DoorNeedingSpell : public Door
+{
+public:
+	DoorNeedingSpell(Room* r1, Room* r2) : Door(r1, r2) {}
+};
+
+
+//---------------- 마법에 걸린 방 구성 요소들. 끝
+
+
+//----------------폭탄이 설치된 방 구성 요소들. 시작
+
+class RoomWithABomb : public Room
+{
+public:
+	RoomWithABomb(int n) : Room(n) {}
+};
+
+class DoorWithABomb : public Door
+{
+public:
+	DoorWithABomb(Room* r1, Room* r2) : Door(r1,r2) {}
+
+};
+
+//----------------폭탄이 설치된 방 구성 요소들. 끝
--- study/KGCA/5_DesignPattern/1_AbstractFactory/afMain.cpp
+++ study/KGCA/5_DesignPattern/1_AbstractFactory/afMain.cpp
@@ -1,12 +1,15 @@
 #include "afHeader.h"
 
+
+//미로의 구성요소들을 생성하는 (기본) 추상 팩토리 클래스
+//이 클래스가 반환하는 각각의 클래스에 대한 정의가 미리 필요하다.
 class MazeFactory
 {
 public:
-	virtual Maze* MakeMaze() const { return new Maze; }
-	virtual Wall* MakeWall() const { return new Wall; }
-	virtual Room* MakeRoom(int n) const { return new Room(n); }
-	virtual Door* MakeDoor(Room* r1, Room* r2) const { return new Door(r1, r2); }
+	virtual Maze* MakeMaze() const { return new Maze; }                              //미로를 만든다.
+	virtual Wall* MakeWall() const { return new Wall; }                              //벽을 만든다.
+	virtual Room* MakeRoom(int n) const { return new Room(n); }                      //방을 만든다.
+	virtual Door* MakeDoor(Room* r1, Room* r2) const { return new Door(r1, r2); }    //선택된 두 방 사이에 문을 만든다.
 
 
 public:
@@ -14,37 +17,32 @@ public:
 	~MazeFactory() {}
 };
 
-class Spell {
-public:
-	Spell() {}
-	~Spell() {}
-};
-
-class EnchantRoom : public Room
-{
-public:
-	EnchantRoom(int n, Spell* sp) : Room(n) {}
-};
-
-class DoorNeedingSpell : public Door
-{
-public:
-	DoorNeedingSpell(Room* r1, Room* r2) : Door(r1,r2) {}
-};
-
+//마법에 걸린 미로를 생성하는 추상 팩토리 클래스
+//미로를 만드는 메소드와 벽을 만드는 메소드는 달라진게 없기 때문에 그냥 상속받아서 사용하고
+//다르게 제작된 방을 만드는 메소드와 문을 메소드만 재정의했다.
 class EnchantedMazeFactory : public MazeFactory
 {
 protected:
 	Spell * CastSpell() const { return new Spell; }
 
 public:
-	Room* MakeRoom(int n) const override
+
+// 아래의 두 함수는 그대로 쓴다.
+//	virtual Maze* MakeMaze() const { return new Maze; }                              //미로를 만든다.
+//	virtual Wall* MakeWall() const { return new Wall; }                              //벽을 만든다.
+
+	//이 클래스에서 사용하는 마법에 걸린 방과 마법에 걸린 문은 각각의 클래스를 상속받아 재정의된 클래스이다.
+
+	Room * MakeRoom(int n) const override                                            //마법에 걸린 방을 만든다.
 	{
-		return new EnchantRoom(n, CastSpell());
+		//room클래스를 상속받은 EnchantedRoom클래스의 인스턴스를 리턴한다.
+		return new EnchantedRoom(n, CastSpell());
 	}
 
-	Door* MakeDoor(Room* r1, Room* r2) const override
+
+	Door* MakeDoor(Room* r1, Room* r2) const override                                //마법에 걸린 문을 만든다.
 	{
+		//Door클래스를 상속받은 DoorNeedingSpell클래스의 인스턴스를 리턴한다.
 		return new DoorNeedingSpell(r1, r2);
 	}
 
@@ -54,9 +52,50 @@ public:
 
 };
 
-class MazeGame
+
+//폭탄이 설치된 미로라고 치면
+class BombedMazeFactory : public MazeFactory
 {
 public:
+	Room* MakeRoom(int n) const override
+	{
+		return new RoomWithABomb(n);
+	}
+
+	Door* MakeDoor(Room* r1, Room* r2) const override 
+	{
+		return new DoorWithABomb(r1, r2);
+	}
+
+public:
+	BombedMazeFactory() {}
+};
+
+
+//미로를 만드는 클래스.
+//복잡한 미로를 만드는 알고리즘은 구현되어 있지 않고,
+//그냥 두방 사이에 문을 만드는 것만 구현되어 있다.
+class MazeGame
+{
+	//팩토리 메서드 패턴이 추가되면 : 
+	// 방, 벽, 문을 생성하기 위해서 생성자를 이용하지 않고 가상 함수를 호출하도록 구현되어 있다면,
+	// 이 가상 함수의 실제 구현을 다양한 방법으로 변경할 수 있다.
+
+	//추상 팩토리 패턴이 추가되면 :
+	// 방, 벽, 문을 생성하기 위한 생성 방법을 알고 있는 객체를 매개 변수로 넘겨받을 수 있다면,
+	// 생성 방법이 바뀔 때마다 새로운 매개변수를 넘겨받음으로써 생성할 객체의 유형을 달리할 수 있다.
+
+	//빌터 패턴이 추가되면 : 
+	// 생성하고자 하는 미로에 방, 문, 벽을 추가하는 연산을 사용해서 새로운 미로를 만들 수 있는 객체를 넘겨받는다면,
+	// 미로를 만드는 방법이나 변경을 이 객체의 상속을 통해서 해결할 수 있다.
+
+	//원형 패턴을 추가되면 :
+	// 이미 만든 다양한 방, 문, 벽 객체를 매개변수화 해두고 이미 만든 객체를 복사해서 미로에 추가하면, 
+	// 이들 인스턴스를 교체하여 미로의 복합 방법을 변경할 수 있다.
+
+public:
+	//디자인 패턴을 사용하지 않은 버전
+	//문제점1 : 다른 구성 요소를 추가해서 미로를 만들기가 어렵다.
 	Maze * CreateMaze()
 	{
 		Maze* aMaze = new Maze;
@@ -79,6 +118,8 @@ public:
 		return aMaze;
 	}
 
+	//추상 클래스 패턴을 사용하는 버전
+	//어떤 추상 팩토리를 사용하느냐에 따라 전혀 다른 컨셉의 미로를 만들 수 있다. 
 	Maze* CreateMaze(MazeFactory* factory)
 	{
 		Maze* aMaze = factory->MakeMaze();
@@ -106,11 +147,19 @@ int main()
 	MazeGame mg;
 	Maze* pMaze = mg.CreateMaze();
 
+	//기본 테마
 	MazeGame mg1;
 	MazeFactory* factory1 = new MazeFactory;
 	Maze* pMaze1 = mg1.CreateMaze(factory1);
 
+	//마법이 걸린 미로 테마
 	MazeGame mg2;
 	MazeFactory* factory2 = new EnchantedMazeFactory;
-	Maze* pMaze2 = mg1.CreateMaze(factory2);
+	Maze* pMaze2 = mg2.CreateMaze(factory2);
+
+	//폭탄이 설치된 미로 테마
+	MazeGame mg3;
+	MazeFactory* factory3 = new BombedMazeFactory;
+	Maze* pMaze3 = mg3.CreateMaze(factory3);
+	
 }
