19062d7b65e199184b5cad5b7437fb8ec0eefce2
김이삭 <kimisaak0@gmail.com>
2018-11-02 02:51:51 +0900

    ???

--- study/3_DataStructure/0_LinearList/Linear_List.h
+++ study/3_DataStructure/0_LinearList/Linear_List.h
@@ -8,14 +8,31 @@ class LinearList
 	T* arr;
 	int iSize;
 	int iEnd;
+	int nowIndex;
 
 	//std::vector<int> vi;
 
+private:
+	void moveData();
+	
+
 public:
+	//인덱스로 접근, 수정이 가능.
 	T& operator[](int index);
 
 	int GetSize();
-	void insert(T data);
+	int GetIndex();
+	int GetEnd();
+
+	int addData(T data);
+
+	int InsertData(int index, T data);
+
+	T& prev();
+	T& now();
+	T& next();
+
+	
 
 public:
 	LinearList();
@@ -23,39 +40,9 @@ public:
 	~LinearList();
 };
 
-//생성자
+//내부 함수
 template <typename T>
-LinearList<T>::LinearList()
-{
-	arr = nullptr;
-	iSize = 0;
-	iEnd = 0;
-}
-
-template <typename T>
-LinearList<T>::LinearList(int size)
-{
-	arr = new T[size];
-	iSize = size;
-	iEnd = 0;
-}
-
-//[] 오버로딩
-template <typename T>
-T& LinearList<T>::operator[](int index)
-{
-	assert(0 <= index);
-	return arr[index];
-}
-
-template <typename T>
-int LinearList<T>::GetSize()
-{
-	return iSize;
-}
-
-template <typename T>
-void LinearList<T>::insert(T data)
+void LinearList<T>::moveData()
 {
 	if (iEnd >= iSize) {
 		T* backup = new T[iSize];
@@ -67,9 +54,105 @@ void LinearList<T>::insert(T data)
 			arr[i] = backup[i];
 		}
 		iSize = iSize + 5;
-	} 
+	}
+}
+
+//생성자
+template <typename T>
+LinearList<T>::LinearList()
+{
+	arr = nullptr;
+	iSize = 0;
+	iEnd = 0;
+	nowIndex = 0;
+}
+
+template <typename T>
+LinearList<T>::LinearList(int size)
+{
+	arr = new T[size];
+	iSize = size;
+	iEnd = size;
+	nowIndex = 0;
+}
+
+//[] 오버로딩
+template <typename T>
+T& LinearList<T>::operator[](int index)
+{
+	assert(0 <= index);
+	return arr[index];
+}
+
+//getMethod
+template <typename T>
+int LinearList<T>::GetSize()
+{
+	return iSize;
+}
+
+template <typename T>
+int  LinearList<T>::GetIndex()
+{
+	return nowIndex;
+}
+
+template <typename T>
+int  LinearList<T>::GetEnd()
+{
+	return iEnd;
+}
+
+//뒤에 추가
+template <typename T>
+int LinearList<T>::addData(T data)
+{
+	moveData();
 
 	arr[iEnd++] = data;
+
+	return iEnd;
+}
+
+//중간에 삽입
+template <typename T>
+int LinearList<T>::InsertData(int index, T data)
+{
+	iEnd++;
+	moveData();
+	for (int i = iEnd; i > index; i--) {
+		arr[iEnd] = arr[iENd-1]
+	}
+
+	arr[index] = data;
+	
+	return iEnd;
+}
+
+
+//순회용.
+template <typename T>
+T& LinearList<T>::prev()
+{
+	if (nowIndex > 0) {
+		return arr[--nowIndex];
+	}
+}
+
+
+template <typename T>
+T& LinearList<T>::now()
+{
+	return arr[nowIndex];
+}
+
+
+template <typename T>
+T& LinearList<T>::next()
+{
+	if (nowIndex < iEnd) {
+		return arr[++nowIndex];
+	}
 }
 
 //소멸자
