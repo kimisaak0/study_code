fb7a67ad70b97a84c46ab3137f0d0a9b4329eb3d
김이삭 <kimisaak0@gmail.com>
2018-11-12 00:58:37 +0900

    bt

--- study/3_DataStructure/16_ABT_pointer/main.cpp
+++ study/3_DataStructure/16_ABT_pointer/main.cpp
@@ -1,6 +1,20 @@
 #include "BinaryTree.h"
+#include <iostream>
+
+void print(int data) {
+	std::cout << data << std::endl;
+}
 
 int main()
 {
+	BinaryTree<int> bt(0);
+	std::cout << CheckData(bt.getRoot()) << std::endl;
+
+	BinaryTree<int> bt2(1);
+
+	bt2.connectLeft(bt2.getRoot(), MakeNode(2));
+	std::cout << CheckData(bt2.getLeft(bt2.getRoot())) << std::endl;
+
+	preorder(bt2.getRoot(), print);
 
 }
